a773c7ebfa5699972853ce1ac0ed2ca231781993
Houta <houtadono@gmail.com>
2022-08-25 07:39:33 +0700

    update code AC

--- Mang/SoXaCach.java
+++ Mang/SoXaCach.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
  * @author Houta
  */
 public class SoXaCach {
-    public static short []arr = new short [11];
-    public static short []vs = new short [11];
-    public static short n,k,res=0;
+    public static int []arr = new int [12];
+    public static int []vs = new int [12];
+    public static int n,k;
     
     public static void in(){
-        for(short i = 1;i<=n;i++)
+        for(int i = 1;i<=n;i++)
             System.out.print(arr[i]);
         System.out.println();
     }
     
-    public static void backtracking(short i){
-        for(short j = 1;j<=n;j++){
+    public static void backtracking(int i){
+        for(int j = 1;j<=n;j++){
             if(vs[j]==0){
                 arr[i] = j;
-                if(Math.abs(arr[i]-arr[i-1])==1) continue;
+                if(Math.abs(arr[i]-arr[i-1])==1&&i>1) continue;
                 vs[j]=1;
                 if(i==n) in();
-                else backtracking((short)(i+1));
+                else backtracking(i+1);
                 vs[j]=0;
             }
         }
@@ -30,12 +30,12 @@ public class SoXaCach {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        short t = sc.nextShort();
+        int t = sc.nextInt();
         while(t>0){
             t--;
-            n = sc.nextShort();
-            backtracking((short)1);
+            n = sc.nextInt();
+            backtracking(1);
         }
-        
+        sc.close();
     }
 }
