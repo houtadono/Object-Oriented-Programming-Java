536bd1c204035e6b3377fc8ccfbecf5af271521f
Houta <houtadono@gmail.com>
2022-08-28 22:53:36 +0700

    update code ac day 7

--- XauKyTu/XauConLonNhat.java
+++ XauKyTu/XauConLonNhat.java
@@ -8,7 +8,7 @@ public class XauConLonNhat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        String res = ""+s.charAt(0);
+        String res = ""+s.charAt(s.length()-1);
         for(int i=s.length()-2;i>=0;i--){
             if(res.length()>0 && s.charAt(i) >= res.charAt(0)  )
                 res = s.charAt(i) + res;
