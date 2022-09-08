869cfe7841e285aa1062dbc69dd512d5bf23e66d
Houta <houtadono@gmail.com>
2022-09-08 23:49:29 +0700

    update code AC day 18

--- MangDoiTuong/SapXepThoiGian.java
+++ MangDoiTuong/SapXepThoiGian.java
@@ -9,26 +9,20 @@ import java.util.Scanner;
 public class SapXepThoiGian {
     private static class Time implements Comparable<Time>{
         private int h,m,s;
-        private String t;
         public Time(int h, int m, int s) {
             this.h = h;
             this.m = m;
             this.s = s;
-            if(h>10) this.t = String.valueOf(h)+' ';
-            else this.t = '0'+String.valueOf(h)+' ';
-            if(m>10) this.t += String.valueOf(m)+' ';
-            else this.t += '0'+String.valueOf(m)+' ';
-            if(s>10) this.t += String.valueOf(s);
-            else this.t += '0'+String.valueOf(s);
         }
 
         @Override
         public int compareTo(Time o) {
-            return this.t.compareTo(o.t);
-        }
-        @Override
-        public String toString() {
-            return this.t;
+            if(this.h>o.h) return 1;
+            if(this.h<o.h) return -1;
+            if(this.m>o.m) return 1;
+            if(this.m<o.m) return -1;
+            if(this.s>o.s) return 1;
+            return -1;
         }
     }
     public static void main(String []args){
@@ -37,6 +31,7 @@ public class SapXepThoiGian {
         ArrayList<Time> ds = new ArrayList<>();
         while(t-->0) ds.add(new Time(sc.nextInt(),sc.nextInt(),sc.nextInt()));
         Collections.sort(ds);
-        for(Time tmp:ds) System.out.println(tmp);
+        for(Time tmp:ds) 
+            System.out.print(tmp.h+" "+tmp.m+" "+tmp.s+"\n");
     }
 }
